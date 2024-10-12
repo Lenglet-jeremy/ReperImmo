d6a2a9a5893b1e002d43082701ed71ea2823981f
Lenglet-jeremy <lengletjeremy.pro@gmail.com>
2024-10-12 08:31:09 +0200

    mise a jour

--- API/server.js
+++ API/server.js
@@ -12,7 +12,15 @@ app.use(cors({
     methods: 'GET,POST,PUT,DELETE',
     allowedHeaders: 'Content-Type,Authorization',
     credentials: true // Si tu veux envoyer des cookies, ajoute cette option
-  }));
+}));
+
+app.use((req, res, next) => {
+res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+res.header('Access-Control-Allow-Credentials', 'true'); // Si tu utilises des cookies
+next();
+});
   
 app.use(express.json());
 
